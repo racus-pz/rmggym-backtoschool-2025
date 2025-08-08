22def9053e33f7697e371f34b3f0ecfe1456ed98
Kajetan Dzioboń <kajtom@gmail.com>
2025-08-08 16:17:28 +0200

    0.3.8 - poprawiony nagłówek hero dla promocji wrześniowej

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -54,8 +54,8 @@ const Index = () => {
             transition={{ duration: 0.8 }}
             className="text-center"
           >
-            <h1 className="text-4xl md:text-6xl font-poppins leading-8 font-extrabold text-white mb-6">
-              Trenuj <span className="text-yellow-400">cały wrzesień</span> za 39 zł<br />i zachowaj dobrą cenę na kolejne miesiące
+            <h1 className="text-4xl md:text-6xl font-poppins leading-10 font-extrabold text-white mb-6">
+              Trenuj <span className="text-yellow-400">cały wrzesień za 39 zł</span><br />i zachowaj dobrą cenę na kolejne miesiące
             </h1>
 
             
